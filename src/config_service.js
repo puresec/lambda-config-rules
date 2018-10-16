9a615f7c1e7131ef09dd1c112fd57684d4eafb28
Yuri Shapira <yuri@puresec.io>
2018-10-16 14:07:14 +0300

    submit evaluations in chunks (PutEvalutions has limit of 100 evaluations)

--- src/config_service.js
+++ src/config_service.js
@@ -4,11 +4,29 @@ const AWS = require('aws-sdk');
 require('./aws_sdk_hooks');
 const config = new AWS.ConfigService({});
 
-async function putEvaluations(params) {
-  let result = await config.putEvaluations(params).promise();
-  if ('FailedEvaluations' in result && result.FailedEvaluations.length > 0) {
-    throw Error(JSON.stringify(result));
+function chunkArray(myArray, chunk_size) {
+  let results = [];
+
+  while (myArray.length) {
+    results.push(myArray.splice(0, chunk_size));
   }
+
+  return results;
+}
+
+async function putEvaluations(params) {
+  let evaluationsChunks = chunkArray(params.Evaluations, 100);
+  await Promise.all(evaluationsChunks.map(async (evaluationsChunk) => {
+    let result = await config.putEvaluations({
+      Evaluations: evaluationsChunk,
+      ResultToken: params.ResultToken,
+      TestMode: params.TestMode
+    }).promise();
+    if ('FailedEvaluations' in result && result.FailedEvaluations.length > 0) {
+      throw Error(JSON.stringify(result));
+    }
+
+  }));
 }
 
 module.exports = {
